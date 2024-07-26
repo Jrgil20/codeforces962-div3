2fcb01236037c7915e5a710ac22582b9519bc622
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-26 10:48:49 -0400

    chore: Add solution for counting legs of animals

---
+++ aLegs.py
@@ -0,0 +1,14 @@
+def Animals(legs):
+    if legs % 2 != 0:
+        return "No solution"
+    elif legs % 4 == 0:
+        return legs // 4
+    else:
+        return legs// 4 + 1
+
+
+n = int(input())
+
+for _ in range(n):
+    num = int(input())
+    print(Animals(num))
